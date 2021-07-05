471bce6635f3fb0ab976e2df78225862bc5eeae0
Zavanven <avanven@gmail.com>
2021-07-05 10:45:45 +0200

    Added simple column and row configuration to address resize problems

--- gui.py
+++ gui.py
@@ -1,5 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+from pathlib import Path
+import pandas as pd
 
 class Application:
     def __init__(self, root: Tk):
@@ -23,13 +27,13 @@ class Application:
         self.select_col_label = Label(self.frame_main, text="Select columns:")
         self.selected_col_label = Label(self.frame_main, text="Selected columns:")
         self.select_col_label.grid(row=0, column=0, sticky=W, pady=2, padx=5)
-        self.selected_col_label.grid(row=0, column=2, sticky=W, pady=2, padx=5)
+        self.selected_col_label.grid(row=0, column=3, sticky=W, pady=2, padx=5)
 
         # Frames for columns
         self.available_col_frame = Frame(self.frame_main, bg="blue")
-        self.available_col_frame.grid(row=1, column=0, rowspan=2)
+        self.available_col_frame.grid(row=1, column=0, columnspan=2, rowspan=2, sticky=(N, S, E, W))
         self.selected_col_frame = Frame(self.frame_main, bg="blue")
-        self.selected_col_frame.grid(row=1, column=2, rowspan=2)
+        self.selected_col_frame.grid(row=1, column=3, columnspan=2, rowspan=2, sticky=(N, S, E, W))
 
         # Scrollbars
         self.available_list_scrollbar = Scrollbar(self.available_col_frame, orient='vertical')
@@ -45,14 +49,24 @@ class Application:
         self.selected_list_scrollbar.pack(side="right", fill="y")
         self.selected_list.pack(side="left", fill="both", expand=1)
 
-        # Fake data
-        self.populate_with_fake_data()
-
         # Buttons
         self.add_col_button = Button(self.frame_main, text=">>", command=self.add_columns)
         self.remove_col_button = Button(self.frame_main, text="<<", command=self.remove_columns)
-        self.add_col_button.grid(row=1, column=1, padx=5)
-        self.remove_col_button.grid(row=2, column=1, padx=5)
+        self.add_col_button.grid(row=1, column=2, padx=5)
+        self.remove_col_button.grid(row=2, column=2, padx=5)
+
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+        self.frame_main.columnconfigure(0, weight=1)
+        self.frame_main.columnconfigure(1, weight=1)
+        self.frame_main.columnconfigure(2, weight=1)
+        self.frame_main.columnconfigure(3, weight=1)
+        self.frame_main.columnconfigure(4, weight=1)
+        self.frame_main.rowconfigure(1, weight=1)
+        self.frame_main.rowconfigure(2, weight=1)
+        # self.frame_main.rowconfigure(3, weight=1)
+        # self.frame_main.rowconfigure(4, weight=1)
+
 
     def add_columns(self):
         """
@@ -88,7 +102,37 @@ class Application:
             self.available_list.itemconfig(each_item)
 
     def open_json_file(self):
-        pass
+        """
+        Open json file and pass it to pandas
+        """
+        file_path = self.select_file()
+        try:
+            df = pd.read_json(Path(file_path), lines=True)
+        except TypeError:
+            return
+        df = pd.json_normalize(df['data'])
+        self.populate_columns_with_data(list(df.columns))
+
+    def populate_columns_with_data(self, list_of_columns):
+        for each_item in range(len(list_of_columns)):    
+            self.available_list.insert(END, list_of_columns[each_item])
+            self.available_list.itemconfig(each_item)
+
+
+    def select_file(self):
+        """
+        Show open file dialog and return path to selected file
+        """
+        filetypes = (
+            ('Json files', '*.json'),
+        )
+
+        filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='.',
+            filetypes=filetypes)
+
+        return filename
 
 
 if __name__ == "__main__":
